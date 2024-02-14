150fd564d9972f83402af6933bf35bba50bdd3b1
Rodrigo <ro6rigo@gmail.com>
2024-02-14 08:45:00 -0300

    adicionando tag GA

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,15 @@ import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import { lusitana } from '@/app/ui/fonts';
 import Link from 'next/link';
 import Image from 'next/image';
-export default function Page() {
+import { initGA, logPageView } from '../scripts/analytics';
+export default function Page() { 
   return (
     <main className="flex min-h-screen flex-col p-6">
+      <script>        
+        initGA();          
+        logPageView();
+      </script>
+      
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
         <AcmeLogo />
       </div>
